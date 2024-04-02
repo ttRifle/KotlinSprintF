fde98bc8bbdeee8a83f515b06d40b678b0ae618a
ttrifle <Bigtoolforyou!@>
2024-04-02 23:12:00 +0300

    lesson 1-4 done

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_1
 
 fun main() {
-    //lesson 1-4
+    
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("""
+        $length
+        $age
+        $partOfDay
+        $seconds
+        $partOfYear
+        $apogee
+    """.trimIndent())
 }
