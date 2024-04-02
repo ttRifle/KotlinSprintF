1429b0d1e769181069d0d44fe846317d37af7d13
ttrifle <Bigtoolforyou!@>
2024-04-02 22:29:31 +0300

    lesson 1-2 done

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -1,5 +1,24 @@
 package org.example.lesson_1
 
+import java.security.Principal
+
 fun main() {
-    //lesson 1-2
+
+    val orderNumbers = 75
+    val textGratitude = "Спасибо за покупку"
+
+    val rectangleHeight: Int = 100
+    val rectangleWidth: Int = 200
+
+    println(orderNumbers)
+    println(textGratitude)
+    println(rectangleHeight)
+    println(rectangleWidth)
+
+    var WorkersNumbers = 2000
+    //println(WorkersNumbers)
+
+    WorkersNumbers--
+    println(WorkersNumbers)
+
 }
