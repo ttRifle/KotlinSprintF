111b468e4d35aabb574a54f0ef8a5b92b1448b62
ttrifle <Bigtoolforyou!@>
2024-04-03 01:06:26 +0300

    lesson 1-5 done

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,5 +1,26 @@
 package org.example.lesson_1
 
 fun main() {
-    //lesson 1-5
+
+    val seconds: Int = 6480
+    val hours: Int = seconds / 3600
+    val minutes: Int
+    val timeSec: Int
+
+    if (seconds < 3600) minutes = seconds / 60
+    else minutes = seconds / 60 - hours * 60
+
+    if (minutes == 0 && hours == 0) timeSec = seconds
+    else if (minutes > 0 && hours == 0) timeSec = seconds - minutes * 60
+         else timeSec = seconds - hours * 3600 - minutes * 60
+
+    val hoursString = if (hours < 10) "0$hours"
+    else "$hours"
+    val minutesString = if (minutes < 10) "0$minutes"
+    else "$minutes"
+    val secsString = if (timeSec < 10) "0$timeSec"
+    else "$timeSec"
+
+    println("$hoursString:$minutesString:$secsString")
+
 }
