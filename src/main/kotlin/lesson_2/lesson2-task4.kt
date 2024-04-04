c5ae1f86fdce390d7b68e23f00a961be8e34f6f9
ttrifle <Bigtoolforyou!@>
2024-04-04 21:20:51 +0300

    Lesson 2-4 done

--- src/main/kotlin/lesson_2/lesson2-task4.kt
+++ src/main/kotlin/lesson_2/lesson2-task4.kt
@@ -2,6 +2,14 @@ package org.example.lesson_2
 
 fun main() {
 
+    val crystalOreCraft = 7
+    val ironOreCraft = 11
+    val percentBuff = 0.2
 
+    val crystalOreBuff = crystalOreCraft * percentBuff
+    val ironOreBuff = ironOreCraft * percentBuff
+
+    println("Бонус на добычу кристаллической руды " + crystalOreBuff.toInt())
+    println("Бонус на добычу железной руды " + ironOreBuff.toInt())
 
 }
