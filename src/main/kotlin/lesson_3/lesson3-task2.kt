f2c5811c735000141a35b946395b532fd3c9151d
ttrifle <Bigtoolforyou!@>
2024-04-04 23:11:02 +0300

    lesson 3 task 3 done

--- src/main/kotlin/lesson_3/lesson3-task2.kt
+++ src/main/kotlin/lesson_3/lesson3-task2.kt
@@ -2,6 +2,16 @@ package org.example.lesson_3
 
 fun main() {
 
+    val gosUserFirstName = "Елена"
+    var gosUserSecondName = "Иванова"
+    val gosUserFamilyName = "Александровна"
+    var gosUserAge = 20
 
+    println("$gosUserSecondName $gosUserFirstName $gosUserFamilyName, $gosUserAge лет")
+
+    gosUserAge = 22
+    gosUserSecondName = "Ушакова"
+
+    println("$gosUserSecondName $gosUserFirstName $gosUserFamilyName, $gosUserAge лет")
 
 }
