fd1b5e0aafe4228a82247a2ee377765a0bc868cc
ttrifle <Bigtoolforyou!@>
2024-04-04 18:13:50 +0300

    lesson 1-3 val to str done

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -5,9 +5,11 @@ fun main() {
     val departureYear = "1961"
     var hour = "09"
     var minute = "07"
+
     println(departureYear)
     println(hour)
     println(minute)
+
     hour = "10"
     minute = "55"
 
