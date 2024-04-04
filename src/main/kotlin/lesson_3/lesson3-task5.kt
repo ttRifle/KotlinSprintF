5006038548a8d5b26068c15c74922b5bcae5b592
ttrifle <Bigtoolforyou!@>
2024-04-05 00:32:23 +0300

    lesson 3 task 5 done

--- src/main/kotlin/lesson_3/lesson3-task5.kt
+++ src/main/kotlin/lesson_3/lesson3-task5.kt
@@ -2,6 +2,16 @@ package org.example.lesson_3
 
 fun main() {
 
+    val turnWayAndNum = "D2-D4;0"
 
+    val splitTurnWayAndNum: List<String> = turnWayAndNum.split("-", ";")
+
+    val turnFrom = splitTurnWayAndNum[0]
+    val turnTo = splitTurnWayAndNum[1]
+    val turnNum = splitTurnWayAndNum[2]
+
+    println(turnFrom)
+    println(turnTo)
+    println(turnNum)
 
 }
