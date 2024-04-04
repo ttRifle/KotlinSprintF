0ce87114c6a2c9ae3505cd02025407afd752355c
ttrifle <Bigtoolforyou!@>
2024-04-04 19:48:58 +0300

    lesson 2-1 done

---
+++ src/main/kotlin/lesson_2/lesson2-task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val arithmeticStudentEnglishGrade: Double = (Student1EnglishGrade.toDouble() + Student2EnglishGrade.toDouble() + Student3EnglishGrade.toDouble() + Student4EnglishGrade.toDouble()) / 4
+
+    println(arithmeticStudentEnglishGrade)
+
+}
+
+    const val Student1EnglishGrade = 3
+    const val Student2EnglishGrade = 4
+    const val Student3EnglishGrade = 3
+    const val Student4EnglishGrade = 5
