23d45739769437194e7952aaaeca8963aa172f43
ttrifle <Bigtoolforyou!@>
2024-04-04 22:03:36 +0300

    Lesson 2-5 done

--- src/main/kotlin/lesson_2/lesson2-task5.kt
+++ src/main/kotlin/lesson_2/lesson2-task5.kt
@@ -2,6 +2,11 @@ package org.example.lesson_2
 
 fun main() {
 
+    var depositSum = 70000
+    var depositYearPercent = 0.167
+    var depositYears = 20
 
+    val FinalSum = String.format("%.3f",depositSum.toDouble() * Math.pow((1 + depositYearPercent),depositYears.toDouble()))
 
+    println(FinalSum)
 }
