3a8babb9b440275a43681e428a637cd4a3b308bc
ttrifle <Bigtoolforyou!@>
2024-04-04 20:10:00 +0300

    Lesson 2-2 done

--- src/main/kotlin/lesson_2/lesson2-task2.kt
+++ src/main/kotlin/lesson_2/lesson2-task2.kt
@@ -2,6 +2,17 @@ package org.example.lesson_2
 
 fun main() {
 
+    val numberCompanyWorkers = 50
+    val numberCompanyInterns = 20
+    val salaryWorker = 30000
+    val salaryIntern = 20000
 
+    val salaryCompanyWorkers = numberCompanyWorkers * salaryWorker
+    val salaryCompany = salaryCompanyWorkers + salaryIntern * numberCompanyInterns
+    val avarageCompanySalary = salaryCompany / (numberCompanyInterns + numberCompanyWorkers)
+
+    println(salaryCompanyWorkers)
+    println(salaryCompany)
+    println(avarageCompanySalary)
 
 }
