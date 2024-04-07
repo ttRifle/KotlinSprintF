02b9b9ef921fc0c78f999a7a32afc6d63fe7f32b
ttrifle <Bigtoolforyou!@>
2024-04-07 21:24:41 +0300

    lesson 4 - 1 added table order request

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -2,6 +2,11 @@ package org.example.lesson_4
 
 fun main() {
 
+    val numberOfOrderedTablesToday = 13
+    val numberOdOrderedTablesTomorrow = 9
 
+    println("Доступность столиков на сегодня: ${numberOfOrderedTablesToday< NUMBEROFTABLES} \nДоступность столиков на завтра: ${numberOdOrderedTablesTomorrow < NUMBEROFTABLES}")
 
 }
+
+const val NUMBEROFTABLES = 13
