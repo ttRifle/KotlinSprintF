ab83a483f8f22e0aa52f00ba9f489cb8e08f8e3e
ttrifle <Bigtoolforyou!@>
2024-04-07 20:47:33 +0300

    lesson 2 task 1 delete const

--- src/main/kotlin/lesson_2/lesson2-task1.kt
+++ src/main/kotlin/lesson_2/lesson2-task1.kt
@@ -2,13 +2,14 @@ package org.example.lesson_2
 
 fun main() {
 
-    val arithmeticStudentEnglishGrade: Double = (Student1EnglishGrade.toDouble() + Student2EnglishGrade.toDouble() + Student3EnglishGrade.toDouble() + Student4EnglishGrade.toDouble()) / 4
+    val Student1EnglishGrade = 3
+    val Student2EnglishGrade = 4
+    val Student3EnglishGrade = 3
+    val Student4EnglishGrade = 5
+
+    val arithmeticStudentEnglishGrade = (Student1EnglishGrade + Student2EnglishGrade + Student3EnglishGrade + Student4EnglishGrade).toDouble() / 4
 
     println(arithmeticStudentEnglishGrade)
 
 }
 
-    const val Student1EnglishGrade = 3
-    const val Student2EnglishGrade = 4
-    const val Student3EnglishGrade = 3
-    const val Student4EnglishGrade = 5
