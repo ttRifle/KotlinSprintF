d941640fa7d7f5003343aa0cb86d2df32e610ddc
ttrifle <Bigtoolforyou!@>
2024-04-08 00:00:04 +0300

    lesson 5 - 1 human check

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -2,6 +2,10 @@ package org.example.lesson_5
 
 fun main() {
 
+    println("Сколько будет 2+3?")
 
+    val userAnswer = readln().toInt()
+
+    if (userAnswer == 5) println("Добро пожаловать!") else println("Доступ запрещен!")
 
 }
