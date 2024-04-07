d3c868f88a8077c8610b6be2fc984c02d880fb75
ttrifle <Bigtoolforyou!@>
2024-04-07 20:55:32 +0300

    lesson 2 task 4 var in percent

--- src/main/kotlin/lesson_2/lesson2-task4.kt
+++ src/main/kotlin/lesson_2/lesson2-task4.kt
@@ -4,10 +4,10 @@ fun main() {
 
     val crystalOreCraft = 7
     val ironOreCraft = 11
-    val percentBuff = 0.2
+    val percentBuff = 20
 
-    val crystalOreBuff = crystalOreCraft * percentBuff
-    val ironOreBuff = ironOreCraft * percentBuff
+    val crystalOreBuff = crystalOreCraft * percentBuff / 100
+    val ironOreBuff = ironOreCraft * percentBuff / 100
 
     println("Бонус на добычу кристаллической руды " + crystalOreBuff.toInt())
     println("Бонус на добычу железной руды " + ironOreBuff.toInt())
