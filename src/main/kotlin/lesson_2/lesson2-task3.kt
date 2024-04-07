3660d6229942fe5ea0ae2d053dc9df12eadb21c9
ttrifle <Bigtoolforyou!@>
2024-04-07 20:51:10 +0300

    lesson 2 task 3 added const

--- src/main/kotlin/lesson_2/lesson2-task3.kt
+++ src/main/kotlin/lesson_2/lesson2-task3.kt
@@ -6,13 +6,15 @@ fun main() {
     val trainDepartureTimeMinute = 39
     val trainWayMinutes = 457
 
-    val trainDepartureInMinutes = trainDepartureTimeHour * 60 + trainDepartureTimeMinute
+    val trainDepartureInMinutes = trainDepartureTimeHour * SECINMINUTE + trainDepartureTimeMinute
     val trainArrivalInMinutes = trainDepartureInMinutes + trainWayMinutes
 
-    val trainArrivalTimeHour = trainArrivalInMinutes / 60
-    val trainArrivalTimeMinute = trainArrivalInMinutes % 60
+    val trainArrivalTimeHour = trainArrivalInMinutes / SECINMINUTE
+    val trainArrivalTimeMinute = trainArrivalInMinutes % SECINMINUTE
 
     val trainArrivalTime = String.format ("%02d:%02d", trainArrivalTimeHour, trainArrivalTimeMinute)
     println(trainArrivalTime)
 
 }
+
+const val SECINMINUTE = 60
