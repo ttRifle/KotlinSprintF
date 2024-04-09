bf194b0c2cde69ff8d4fed697cf4ffd54bb1f735
ttrifle <Bigtoolforyou!@>
2024-04-09 22:17:37 +0300

    lesson 4 - 3 const added, value boolean name changed

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,19 @@
 package org.example.lesson_4
 
+import java.security.KeyStore.TrustedCertificateEntry
+
 fun main() {
 
-    val whetherSunny = true
-    val tentOpen = true
+    val isSunny = true
+    val isAwningOpen = true
     val airWarmInPercent = 20
     val seasonTime = "зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(whetherSunny == true) && (tentOpen == true) && (airWarmInPercent == 20) && (seasonTime != "зима")}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${(isSunny == IS_FOR_BEAN_SUNNY) && (isAwningOpen == IS_FOR_BEAN_AWNING_OPEN) && (airWarmInPercent == BEAM_AIR_WARM_IN_PERCENT) && (seasonTime != BEAM_SEASON_TIME)}")
 
 }
+
+const val IS_FOR_BEAN_SUNNY = true
+const val IS_FOR_BEAN_AWNING_OPEN = true
+const val BEAM_AIR_WARM_IN_PERCENT = 20
+const val BEAM_SEASON_TIME = "зима"
