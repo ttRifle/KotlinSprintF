586aacf932778a2720cb14f086caf190929d179f
ttrifle <Bigtoolforyou!@>
2024-04-11 21:07:08 +0300

    lesson 5 - 1 vals added

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -2,13 +2,14 @@ package org.example.lesson_5
 
 fun main() {
 
-    val correctAnswer = 5
+    val answerNumber1 = 2
+    val answerNumber2 = 3
 
-    println("Сколько будет 2+3?")
+    println("Сколько будет $answerNumber1+$answerNumber2?")
 
     val userAnswer = readln().toInt()
 
-    if (userAnswer == correctAnswer)
+    if (userAnswer == (answerNumber1 + answerNumber2))
         println("Добро пожаловать!")
     else
         println("Доступ запрещен!")
