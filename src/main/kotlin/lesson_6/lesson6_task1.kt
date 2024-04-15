b250c112400b7308aef94f12760c71244a9bf09d
ttrifle <Bigtoolforyou!@>
2024-04-15 20:59:56 +0300

    lesson 6-1 done

--- src/main/kotlin/lesson_6/lesson6_task1.kt
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -1,5 +1,21 @@
 fun main() {
 
+    println("Введите Логин")
+    val newUserName: String = readln()
 
+    println("Введите пароль")
+    val newPassword: String = readln()
+
+    println("Пользовать $newUserName зарегистрирован!")
+
+    do {
+
+        println("Для авторизации введите логин")
+        val userNameCheck: String = readln()
+
+        println("введите пароль")
+        val userPassCheck: String = readln()
+
+    } while ((newUserName != userNameCheck) || (newPassword != userPassCheck))
 
 }
