8618c15f7a034943c74fac34c772108a363766b3
ttrifle <Bigtoolforyou!@>
2024-04-15 22:12:04 +0300

    lesson 6-5 done

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -1,5 +1,25 @@
 fun main() {
 
+    var numberOfTry = 3
 
+    while (numberOfTry > 0) {
+
+        numberOfTry--
+
+        val checkNumber1 = (1..9).random()
+        val checkNumber2 = (1..9).random()
+        val sumOfCheckNumbers = checkNumber1 + checkNumber2
+
+        println("Сколько будет $checkNumber1 + $checkNumber2")
+        val userNumber = readln().toInt()
+
+        if (userNumber == sumOfCheckNumbers) {
+            println("Добро пожаловать!")
+            return
+        } else println("Неверно!")
+
+    }
+
+    println("Доступ запрещен!")
 
 }
