c12c973158554b726266aa6fab09b8513e7a61ee
ttrifle <Bigtoolforyou!@>
2024-04-15 21:54:08 +0300

    lesson 6-4 done

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -1,5 +1,25 @@
+import kotlin.random.Random
+
 fun main() {
 
+    var numberOfTry = 5
+    val secretNumber = (0..9).random()
+    println(secretNumber)
 
+    while (numberOfTry > 0) {
+        println("Угадайте число от 1 до 9")
+        println("Осталось попыток: ${numberOfTry--}")
+        val userNumber = readln().toInt()
+        if (userNumber == secretNumber) {
+            println("Это была великолепная игра!")
+            return
+        } else println("Неверно!")
+
+    }
+
+    println("Загаданное число - $secretNumber")
 
 }
+
+
+
