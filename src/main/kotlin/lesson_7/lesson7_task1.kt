09cd2e8069b2b3bc580c0ed44ae8acda8bd4e9da
ttrifle <Bigtoolforyou!@>
2024-04-16 20:34:35 +0300

    lesson 7 - 1 done

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,5 +1,10 @@
 fun main() {
 
+    val passNumberRange = 1 .. 9
+    val passCharRange = 'a' .. 'z'
 
+    val password = passCharRange.random() + passNumberRange.random().toString() + passCharRange.random() + passNumberRange.random().toString() + passCharRange.random() + passNumberRange.random().toString()
+
+    println(password)
 
 }
