c12ce3ba52daa73834423c982e482a421475984f
ttrifle <Bigtoolforyou!@>
2024-04-16 22:47:37 +0300

    lesson 7 - 5  done

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,5 +1,30 @@
 fun main() {
 
+    val rangeUpperCase = 'A'..'Z'
+    val rangeLowerCase = 'a'..'z'
+    val rangeNumbers = 0..9
+    val minPassLenght = 6
+    val numberOfSymbolTypes = 1..3
+    var password: String = ""
 
+    println("Введите длину пароля который хотите сгенерировать(минимальная длина пароля 6 символов")
+    var passLenght = readln().toInt()
+    if (passLenght < minPassLenght) passLenght = 6
+
+    for (i in 1..passLenght) {
+
+        val symbolType = numberOfSymbolTypes.random()
+
+        val symbol: String = when (symbolType) {
+            1 -> rangeUpperCase.random().toString()
+            2 -> rangeLowerCase.random().toString()
+            else -> rangeNumbers.random().toString()
+        }
+
+        password += symbol
+
+    }
+
+    println(password)
 
 }
