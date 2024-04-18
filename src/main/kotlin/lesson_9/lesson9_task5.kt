d7ede157b29718cf6df4e042287342c44168a799
ttrifle <Bigtoolforyou!@>
2024-04-19 01:10:51 +0300

    lesson 9 - 5 done

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,5 +1,21 @@
 fun main() {
 
+    val listOfIngredientsFinalFormat: MutableList<String> = mutableListOf()
+    val listOfIngredients: MutableList<String> = mutableListOf()
+    val numberOfIngredients = 5
 
+    println("Введите 5 ингридиентов вареной картошки")
+    for (i in 0..<numberOfIngredients) {
+        listOfIngredients.add(readln())
+    }
+
+    val listOfIngredientsDistinct = listOfIngredients.distinct().sorted()
+
+    listOfIngredientsDistinct.indices.forEach() {
+        if (it != 0) listOfIngredientsFinalFormat.add(listOfIngredientsDistinct[it])
+        else listOfIngredientsFinalFormat.add(listOfIngredientsDistinct[it].capitalize())
+    }
+
+    println(listOfIngredientsFinalFormat)
 
 }
