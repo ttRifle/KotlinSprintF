3b9391683661f41b49da68058de701969aed359d
ttrifle <Bigtoolforyou!@>
2024-04-18 22:21:20 +0300

    lesson 9 - 2 done

--- src/main/kotlin/lesson_9/lesson9_task2.kt
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -1,5 +1,14 @@
 fun main() {
 
+    val listOfBusinessIdeas = mutableListOf("спать", "есть", "сидеть")
 
+    println("В рецепте есть базовые ингредиенты: $listOfBusinessIdeas")
+
+    println("Желаете добавить еще?")
+    if (readln() == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        listOfBusinessIdeas.add(readln())
+        println("Теперь в рецепте есть следующие ингредиенты: $listOfBusinessIdeas")
+    }
 
 }
