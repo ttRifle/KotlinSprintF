66d13443771af9e6cc6373760904c880de54f4a0
ttrifle <Bigtoolforyou!@>
2024-04-19 16:14:33 +0300

    lesson 10-2 done

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,5 +1,19 @@
 fun main() {
 
+    val passAndLoginMinLenght = 4
 
+    println("Придумай логин(не менее 4х символов")
+    val userName = readln()
+
+    println("Придумай пароль(не менее 4х символов")
+    val userPass = readln()
+
+    println(getLenghtCheck(passAndLoginMinLenght, userName, userPass))
+
+}
+
+fun getLenghtCheck(lenght: Int, userName: String, userPass: String): String {
+    return if ((userName.length < lenght) || (userPass.length < lenght)) "Логин или пароль недостаточно длинные"
+    else "Добро пожаловать"
 
 }
