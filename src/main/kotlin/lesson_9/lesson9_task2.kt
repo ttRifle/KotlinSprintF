81276e177a1320b461567d2356ed4d859aaebe49
ttrifle <Bigtoolforyou!@>
2024-04-20 19:08:22 +0300

    lesson 9-2 with .equals

--- src/main/kotlin/lesson_9/lesson9_task2.kt
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -5,7 +5,8 @@ fun main() {
     println("В рецепте есть базовые ингредиенты: $listOfBusinessIdeas")
 
     println("Желаете добавить еще?")
-    if (readln() == "да") {
+
+    if (readln().equals("да", ignoreCase = true)) {
         println("Какой ингредиент вы хотите добавить?")
         listOfBusinessIdeas.add(readln())
         println("Теперь в рецепте есть следующие ингредиенты: $listOfBusinessIdeas")
