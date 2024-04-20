9a8da35bce645f10c63f11ef8f51059a50f1f707
ttrifle <Bigtoolforyou!@>
2024-04-21 01:36:08 +0300

    lesson 10-2 fun boolean return

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,19 +1,27 @@
 fun main() {
 
-    val passAndLoginMinLenght = 4
-
-    println("Придумай логин(не менее 4х символов")
+    println("Придумай логин(не менее 4х символов)")
     val userName = readln()
 
-    println("Придумай пароль(не менее 4х символов")
-    val userPass = readln()
+    if (isLenghtValid(userName)) {
+        println("Придумай пароль(не менее 4х символов)")
+        val userPass = readln()
 
-    println(getLenghtCheck(passAndLoginMinLenght, userName, userPass))
+        if (isLenghtValid(userPass)) {
+            println("Пользователь зарегистрирован!")
+        } else {
+            println("Пароль должен быть более 4х символов")
+            return
+        }
 
+    } else {
+        println("Логин должен быть более 4х символов")
+        return
+    }
 }
 
-fun getLenghtCheck(lenght: Int, userName: String, userPass: String): String {
-    return if ((userName.length < lenght) || (userPass.length < lenght)) "Логин или пароль недостаточно длинные"
-    else "Добро пожаловать"
+const val PASS_AND_LOGIN_MIN_LENGHT = 4
 
-}
+fun isLenghtValid(toCheck: String): Boolean = toCheck.length >= PASS_AND_LOGIN_MIN_LENGHT
+
+
