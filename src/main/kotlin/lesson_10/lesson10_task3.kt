52ae2d1311aae974de8ab6769b6161635eb82379
ttrifle <Bigtoolforyou!@>
2024-04-20 18:55:15 +0300

    lesson 10-3 done

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -1,5 +1,25 @@
 fun main() {
 
+    println("Введите длину пароля")
+    val passLenght = readln().toInt()
 
+    val userPassword = getPassword(passLenght)
+
+    println(userPassword)
+
+}
+
+fun getPassword(lenght: Int): String {
+
+    val passNumSymbols = (0..9)
+    val passSpecSymbols = "!\"#$%&'()*+,-./ "
+    var password = ""
+
+    for (i in 1..lenght)
+        if (i % 2 == 0) password += passSpecSymbols.random()
+        else password += passNumSymbols.random()
+
+
+    return password
 
 }
