8681f807103725006779906b9354f704d7b2de4c
ttrifle <Bigtoolforyou!@>
2024-04-21 02:26:05 +0300

    lesson 10-2 fun boolean return

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -1,5 +1,36 @@
 fun main() {
 
+    var userWantPlayAnswer = "да"
+    var numberOfUserWins = 0
 
+    while (userWantPlayAnswer.equals("да", ignoreCase = true)) {
+        if (isUserWin()) numberOfUserWins++
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        userWantPlayAnswer = readln()
+    }
+
+    println("Вы выиграли партий: $numberOfUserWins")
+
+}
+
+fun getCubeThrow1(): Int = (1..6).random()
+
+fun isUserWin(): Boolean {
+
+    val compThrowResult = getCubeThrow1()
+    println("Компьютер выбросил $compThrowResult")
+
+    val userThrowResult = getCubeThrow1()
+    println("Пользователь выбросил $userThrowResult")
+
+    val gameResult = when {
+        compThrowResult > userThrowResult -> "Победила машина"
+        compThrowResult < userThrowResult -> "Победило человечество"
+        else -> "Ничья"
+    }
+
+    println(gameResult)
+
+    return (userThrowResult > compThrowResult)
 
 }
