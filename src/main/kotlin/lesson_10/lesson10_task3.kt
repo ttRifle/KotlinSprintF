844928724f186dc3576f45f8c13bc296adcd5e2f
ttrifle <Bigtoolforyou!@>
2024-04-21 21:44:43 +0300

    lesson 10-3 + rangeOfSpecSymbols

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -11,14 +11,13 @@ fun main() {
 
 fun getPassword(lenght: Int): String {
 
-    val passNumSymbols = (0..9)
-    val passSpecSymbols = "!\"#$%&'()*+,-./ "
+    val rangeOfPassNumSymbols = (0..9)
+    val rangeOfPassSpecSymbols = (' '..'/')
     var password = ""
 
     for (i in 1..lenght)
-        if (i % 2 == 0) password += passSpecSymbols.random()
-        else password += passNumSymbols.random()
-
+        if (i % 2 == 0) password += rangeOfPassSpecSymbols.random()
+        else password += rangeOfPassNumSymbols.random()
 
     return password
 
