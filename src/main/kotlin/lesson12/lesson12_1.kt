ab8e12f3a123e379d945e65b1ec0309105cbd5b5
ttrifle <Bigtoolforyou!@>
2024-04-25 01:01:20 +0300

    lesson 12-1 done

--- src/main/kotlin/lesson12/lesson12_1.kt
+++ src/main/kotlin/lesson12/lesson12_1.kt
@@ -1,2 +1,43 @@
 package lesson12
 
+class DayWhether(
+    val dayTemperature: Int = 12,
+    val nightTemperature: Int = 2,
+    val isRainfall: Boolean = true,
+    val isCloudy: Boolean = true,
+) {
+
+
+    fun printWhether() {
+
+        println(
+            """
+            |Температура днем: $dayTemperature 
+            |Температура ночь: $nightTemperature
+            |Осадки: $isRainfall""".trimMargin()
+        )
+
+    }
+
+}
+
+fun main() {
+
+    val dayWhether1 = DayWhether(
+        dayTemperature = 11,
+        nightTemperature = 3,
+        isRainfall = false,
+        isCloudy = false,
+    )
+
+    val dayWhether2 = DayWhether(
+        dayTemperature = 14,
+        nightTemperature = 4,
+        isCloudy = false,
+    )
+
+    dayWhether1.printWhether()
+    dayWhether2.printWhether()
+
+
+}
