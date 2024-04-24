470683f4342668d365ed9d515a1f6a59659a13c3
ttrifle <Bigtoolforyou!@>
2024-04-24 23:26:51 +0300

    lesson 11-5 done

--- src/main/kotlin/lesson11_1/lesson11_1.kt
+++ src/main/kotlin/lesson11_1/lesson11_1.kt
@@ -1,2 +1,91 @@
 package lesson11_1
 
+class ForumMember(
+    var userId: Int = 0,
+    var userName: String? = null,
+) {
+
+    fun createNewMember(newUserName: String): ForumMember {
+        userId++
+        userName = newUserName
+        return ForumMember()
+    }
+}
+
+class Forum(
+    var forumThreadId: Int = 0,
+    var forumUsers: MutableList<String> = mutableListOf(),
+    var forumMessages: MutableList<String> = mutableListOf()
+) {
+    fun printThread() {
+
+        for (i in 0..<forumMessages.size) println("${forumUsers[i]}: ${forumMessages[i]}")
+
+    }
+}
+
+
+class ForumMsg(
+    var authorId: Int = 0,
+    var message: String? = null,
+) {
+
+    fun createNewMsg(userId: Int): ForumMsg {
+        authorId = userId
+        println("Введите сообщение")
+        message = readln()
+        return ForumMsg()
+    }
+
+}
+
+fun main() {
+
+    val userName1 = "Boris"
+    val userName2 = "Alex"
+    val forumMember = ForumMember()
+    val forum = Forum()
+    val forumMsg = ForumMsg()
+
+    forumMember.createNewMember(userName1)
+
+    forum.forumUsers.add(forumMember.userName.toString())
+
+    forumMsg.createNewMsg(forumMember.userId)
+
+    if (forum.forumUsers.contains(forumMember.userName)) {
+        forum.forumMessages.add(forumMsg.message.toString())
+        forum.forumThreadId++
+    }
+
+    forumMsg.createNewMsg(forumMember.userId)
+
+    if (forum.forumUsers.contains(forumMember.userName)) {
+        forum.forumMessages.add(forumMsg.message.toString())
+        forum.forumThreadId++
+        forum.forumUsers.add(forumMember.userName.toString())
+    }
+
+    forumMember.createNewMember(userName2)
+
+    forum.forumUsers.add(forumMember.userName.toString())
+
+    forumMsg.createNewMsg(forumMember.userId)
+
+    if (forum.forumUsers.contains(forumMember.userName)) {
+        forum.forumMessages.add(forumMsg.message.toString())
+        forum.forumThreadId++
+    }
+
+    forumMsg.createNewMsg(forumMember.userId)
+
+    if (forum.forumUsers.contains(forumMember.userName)) {
+        forum.forumMessages.add(forumMsg.message.toString())
+        forum.forumThreadId++
+        forum.forumUsers.add(forumMember.userName.toString())
+    }
+
+    forum.printThread()
+
+}
+
