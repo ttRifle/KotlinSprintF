3c77fd0864d6d8b8cb388ad4f36bf3177392bd05
ttrifle <Bigtoolforyou!@>
2024-04-28 23:30:10 +0300

    12_4 done

--- src/main/kotlin/lesson12/lesson12_4.kt
+++ src/main/kotlin/lesson12/lesson12_4.kt
@@ -1,20 +1,11 @@
 package lesson12
 
-class DayWhetherNew(dayTemperature: Int, nightTemperature: Int, isRainfall: Boolean) {
-
-    var dayTemperature: Int = 280
-    var nightTemperature: Int = 278
-    var isRainfall: Boolean = true
+class DayWhetherNew2(var dayTemperature: Int, var nightTemperature: Int, var isRainfall: Boolean) {
 
     init {
 
         this.dayTemperature = dayTemperature - KEL_TO_CEL.toInt()
         this.nightTemperature = nightTemperature - KEL_TO_CEL.toInt()
-
-    }
-
-
-    fun printWhether1() {
 
         println(
             """
@@ -22,15 +13,14 @@ class DayWhetherNew(dayTemperature: Int, nightTemperature: Int, isRainfall: Bool
             |Температура ночь: $nightTemperature
             |Осадки: $isRainfall""".trimMargin()
         )
-    }
-}
 
+    }
+
+}
 
 fun main() {
 
-    val dayWhether = DayWhetherNew(280, 278, false)
-
-    println(dayWhether.printWhether1())
+    val dayWhether2 = DayWhetherNew2(280, 278, false)
 
 }
 
