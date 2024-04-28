5d41705c2f741874c7d0b9ab6484dfd83f53a5c1
ttrifle <Bigtoolforyou!@>
2024-04-29 02:58:42 +0300

    13_1 done

--- src/main/kotlin/lesson13/lesson13_1.kt
+++ src/main/kotlin/lesson13/lesson13_1.kt
@@ -1,2 +1,21 @@
 package lesson13
 
+class PhoneDir(_name: String, _phoneNumber: Long) {
+
+    val name = _name
+    val phoneNumber = _phoneNumber
+    var company: String? = String()
+
+    constructor(
+        name: String,
+        phoneNumber: Long,
+        company: String?
+    ) : this(name, phoneNumber)
+
+}
+
+
+fun main() {
+
+
+}
