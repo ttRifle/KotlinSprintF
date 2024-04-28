1f6e1166ec821b127a9c6ca6692c37afc5efa379
ttrifle <Bigtoolforyou!@>
2024-04-28 15:13:20 +0300

    lesson 11-5

--- src/main/kotlin/lesson11_1/lesson11_5.kt
+++ src/main/kotlin/lesson11_1/lesson11_5.kt
@@ -1,90 +1,89 @@
 class ForumMember(
-    var userId: Int = -1,
-    var userName: String = "",
+    var userId: Int,
+    var userName: String,
 )
 
 class Forum(
-    var forumThreadId: Int = -1,
     var forumMembers: MutableList<ForumMember> = mutableListOf(),
     var forumMessages: MutableList<ForumMsg> = mutableListOf(),
 ) {
 
-    fun createNewUser(forumMember: ForumMember): ForumMember {
+    fun createNewUsers() {
 
-        println("Введите имя нового пользователя")
-        forumMember.userName = readln()
-        forumThreadId++
-        forumMember.userId = forumThreadId
-        forumMembers.add(forumMember)
-        return forumMember
+        val forumMember1 = ForumMember(0, "Boris")
+
+        forumMembers.add(forumMember1)
+
+        val forumMember2 = ForumMember(1, "Moris")
+
+        forumMembers.add(forumMember2)
 
     }
 
     fun printThread() {
 
         for (i in 0..<forumMessages.size) {
+
             println("${forumMembers[forumMessages[i].authorId].userName}: ${forumMessages[i].message}")
+
         }
+
 
     }
 
-    fun createNewMessage(forumMsg: ForumMsg): ForumMsg {
+    fun createNewMessage() {
 
-        println("Введите ID пользователя для отправки сообщения")
-        val id = readln().toInt()
+
+        val forumMsg1 = ForumMsg(0, "Привет")
 
         for (i in 0..<forumMembers.size) {
 
-            if (forumMembers[i].userId == id) {
-
-                println("Введите сообщение от пользователя ${forumMembers[i].userName}")
-                forumMsg.message = readln()
-                forumMsg.authorId = forumMembers[i].userId
-                forumMessages.add(forumMsg)
-                return forumMsg
-            }
+            if (forumMsg1.authorId == forumMembers[i].userId) forumMessages.add(forumMsg1)
 
         }
 
-        println("Пользователь с id: $id не найден")
+        val forumMsg2 = ForumMsg(1, "Привет")
 
-        return forumMsg
+        for (i in 0..<forumMembers.size) {
+
+            if (forumMsg2.authorId == forumMembers[i].userId) forumMessages.add(forumMsg2)
+
+        }
+
+        val forumMsg3 = ForumMsg(0, "Пока")
+
+        for (i in 0..<forumMembers.size) {
+
+            if (forumMsg3.authorId == forumMembers[i].userId) forumMessages.add(forumMsg3)
+
+        }
+
+        val forumMsg4 = ForumMsg(1, "Пока")
+
+        for (i in 0..<forumMembers.size) {
+
+            if (forumMsg4.authorId == forumMembers[i].userId) forumMessages.add(forumMsg4)
+
+        }
+
     }
 
 }
 
 
 class ForumMsg(
-    var authorId: Int = -1,
-    var message: String = "",
+    var authorId: Int,
+    var message: String,
 )
 
 fun main() {
 
-    var forumMember1 = ForumMember()
-    var forumMember2 = ForumMember()
-
-    var forumMsg1 = ForumMsg()
-    var forumMsg2 = ForumMsg()
-    var forumMsg3 = ForumMsg()
-    var forumMsg4 = ForumMsg()
-
     val forum1 = Forum()
 
-    forumMember1 = forum1.createNewUser(forumMember1)
+    forum1.createNewUsers()
 
-    forumMember2 = forum1.createNewUser(forumMember2)
-
-    forumMsg1 = forum1.createNewMessage(forumMsg1)
-
-    forumMsg2 = forum1.createNewMessage(forumMsg2)
-
-    forumMsg3 = forum1.createNewMessage(forumMsg3)
-
-    forumMsg4 = forum1.createNewMessage(forumMsg4)
+    forum1.createNewMessage()
 
     forum1.printThread()
 
-
 }
-
