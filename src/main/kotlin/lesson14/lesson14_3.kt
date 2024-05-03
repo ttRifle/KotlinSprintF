8c2111efc33d8d8e625587cd43460dd4ec8ac9ff
ttrifle <Bigtoolforyou!@>
2024-05-04 00:26:32 +0300

    lesson 14-3 change

--- src/main/kotlin/lesson14/lesson14_3.kt
+++ src/main/kotlin/lesson14/lesson14_3.kt
@@ -2,87 +2,93 @@ package lesson14
 
 abstract class Figure(
     val color: String,
-    var square: Int,
-    var perimeter: Int,
 ) {
 
-    abstract fun squareCalculation()
-    abstract fun perimeterCalculation()
+    abstract fun calculateSquare(): Double
+    abstract fun calculatePerimeter(): Double
 
 }
 
 class Circle(
     color: String,
-    val radius: Int = 0,
-    square: Int = 0,
-    perimeter: Int = 0,
-) : Figure(color, square, perimeter) {
-    override fun squareCalculation() {
-        square = (radius * PI).toInt()
+    val radius: Double = 0.0,
+    var square: Double = 0.0,
+    var perimeter: Double = 0.0,
+) : Figure(color) {
+    override fun calculateSquare(): Double {
+        square = radius * kotlin.math.PI
+        return square
     }
 
-    override fun perimeterCalculation() {
-        perimeter = (2 * PI * radius).toInt()
+    override fun calculatePerimeter(): Double {
+        perimeter = 2 * kotlin.math.PI * radius
+        return perimeter
     }
 
 }
 
 class Rectangle(
     color: String,
-    val height: Int = 0,
-    val width: Int = 0,
-    square: Int = 0,
-    perimeter: Int = 0,
+    val height: Double = 0.0,
+    val width: Double = 0.0,
+    var square: Double = 0.0,
+    var perimeter: Double = 0.0,
 
-    ) : Figure(color, square, perimeter) {
-    override fun squareCalculation() {
+    ) : Figure(color) {
+    override fun calculateSquare(): Double {
         square = height * width
+        return square
     }
 
-    override fun perimeterCalculation() {
+    override fun calculatePerimeter(): Double {
         perimeter = (height + width) * 2
+        return perimeter
     }
 
 }
 
 fun main() {
 
-    val circle1 = Circle("black", 10)
-    val circle2 = Circle("white", 12)
-    val rectangle1 = Rectangle("white", 5, 5)
-    val rectangle2 = Rectangle("black", 6, 6)
+    val circle1 = Circle(FIGURE_COLOR_1, 10.0)
+    val circle2 = Circle(FIGURE_COLOR_2, 12.0)
+    val rectangle1 = Rectangle(FIGURE_COLOR_2, 5.0, 5.0)
+    val rectangle2 = Rectangle(FIGURE_COLOR_1, 6.0, 6.0)
 
-    val listOfFigures = listOf(
-        circle1.also {
-            it.squareCalculation()
-            it.perimeterCalculation()
-        },
+    circle1.square = circle1.calculateSquare()
+    circle1.perimeter = circle1.calculatePerimeter()
+    circle2.square = circle2.calculateSquare()
+    circle2.perimeter = circle2.calculatePerimeter()
+    rectangle1.square = rectangle1.calculateSquare()
+    rectangle1.perimeter = rectangle1.calculatePerimeter()
+    rectangle2.square = rectangle2.calculateSquare()
+    rectangle2.perimeter = rectangle2.calculatePerimeter()
 
-        circle2.also {
-            it.squareCalculation()
-            it.perimeterCalculation()
-        },
-
-        rectangle1.also {
-            it.squareCalculation()
-            it.perimeterCalculation()
-        },
-
-        rectangle2.also {
-            it.squareCalculation()
-            it.perimeterCalculation()
-        }
+    val listOfCircles = listOf(
+        circle1,
+        circle2,
     )
 
-
-    println(
-        "Сумма периметров всех черных фигур: ${listOfFigures.filter { it.color == "black" }.sumOf { it.perimeter }}"
+    val listOfRectangles = listOf(
+        rectangle1,
+        rectangle2,
     )
 
-    println(
-        "Сумма площадей всех белых фигур: ${listOfFigures.filter { it.color == "white" }.sumOf { it.square }}"
-    )
+    val perimeterBlackFigures =
+        listOfCircles.filter { it.color == FIGURE_COLOR_1 }
+            .sumOf { it.perimeter } + listOfRectangles.filter { it.color == FIGURE_COLOR_1 }.sumOf { it.perimeter }
+
+    val squareWhiteFigures =
+        listOfCircles.filter { it.color == FIGURE_COLOR_2 }
+            .sumOf { it.square } + listOfRectangles.filter { it.color == FIGURE_COLOR_2 }.sumOf { it.square }
+
+
+    println("Сумма периметров всех черных фигур: ${String.format("%.2f", perimeterBlackFigures)}")
+
+    println("Сумма площадей всех белых фигур: ${String.format("%.2f", squareWhiteFigures)}")
 
 }
 
-const val PI = 3.14
+const val FIGURE_COLOR_1 = "black"
+
+const val FIGURE_COLOR_2 = "white"
+
