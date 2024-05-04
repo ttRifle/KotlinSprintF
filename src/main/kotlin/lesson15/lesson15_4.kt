dc999ecc7543a02015da143978756828aa44c414
ttrifle <Bigtoolforyou!@>
2024-05-05 01:15:54 +0300

    lesson 15-4 done

--- src/main/kotlin/lesson15/lesson15_4.kt
+++ src/main/kotlin/lesson15/lesson15_4.kt
@@ -1,2 +1,34 @@
 package lesson15
 
+abstract class MusicShop {
+    abstract val name: String
+    abstract val storageQuantity: Int
+}
+
+class Instruments(
+    override val name: String,
+    override val storageQuantity: Int,
+    val listOfParts: List<Parts> = listOf(),
+) : MusicShop(), Searchable
+
+class Parts(
+    override val name: String,
+    override val storageQuantity: Int
+) : MusicShop()
+
+interface Searchable {
+
+    fun searchByName() {
+        println("Производится поиск")
+    }
+
+}
+
+fun main() {
+
+    val parts = Parts("Трость", 100)
+    val instruments = Instruments("Кларнет", 100, listOf(parts))
+
+    instruments.searchByName()
+
+}
