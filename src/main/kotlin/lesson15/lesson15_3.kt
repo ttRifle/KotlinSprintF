c488625b4745a732f1c5142716a60c02a9a048c3
ttrifle <Bigtoolforyou!@>
2024-05-05 00:57:30 +0300

    lesson 15-3 done

--- src/main/kotlin/lesson15/lesson15_3.kt
+++ src/main/kotlin/lesson15/lesson15_3.kt
@@ -1,2 +1,69 @@
 package lesson15
 
+abstract class ForumMembers5 : Userable {
+
+    abstract val userId: Int
+    abstract val username: String
+    abstract val password: String
+    abstract val dateOfBirth: String
+
+    fun loginForum() {
+        println("Пользователь авторизирован")
+    }
+}
+
+class User(
+    override val userId: Int,
+    override val username: String,
+    override val password: String,
+    override val dateOfBirth: String,
+) : ForumMembers5()
+
+class Admin(
+    override val userId: Int,
+    override val username: String,
+    override val password: String,
+    override val dateOfBirth: String,
+    val isAdmin: Boolean = true,
+) : ForumMembers5(), Adminable
+
+interface Userable {
+
+    fun readMessage() {
+        println("Сообщение прочитано")
+    }
+
+    fun writeMessage() {
+        println("Сообщение написано")
+    }
+
+}
+
+interface Adminable {
+
+    fun deleteMessage() {
+        println("Сообщение удалено")
+    }
+
+    fun deleteUser() {
+        println("Пользователь удален")
+    }
+
+}
+
+
+fun main() {
+    val admin = Admin(1, "1", "1", "1")
+    val user = User(2, "2", "2", "2")
+
+    admin.loginForum()
+    admin.readMessage()
+    admin.writeMessage()
+    admin.deleteUser()
+    admin.deleteMessage()
+
+    user.loginForum()
+    user.readMessage()
+    user.writeMessage()
+
+}
