67335ddb8588dca23c23f263ab82c6945670e8ce
ttrifle <Bigtoolforyou!@>
2024-05-04 03:41:33 +0300

    lesson 15-2 done

--- src/main/kotlin/lesson15/lesson15_2.kt
+++ src/main/kotlin/lesson15/lesson15_2.kt
@@ -1,2 +1,46 @@
 package lesson15
 
+abstract class WeatherStationStats {
+
+    abstract val timeOfDay: String
+
+}
+
+class Temperature(
+    override val timeOfDay: String,
+    val temp: Int,
+) : WeatherStationStats() {
+
+}
+
+class PrecipitationAmount(
+    override val timeOfDay: String,
+    val precipitationAmount: Int,
+) : WeatherStationStats() {
+
+}
+
+class WeatherServer(
+    val timeOfDay: String = String(),
+    val temperature: Int? = null,
+    val precipitationAmount: Int? = null,
+) {
+
+    fun sentServerSMS(temperature: Temperature, precipitationAmount: PrecipitationAmount): WeatherServer {
+        val weatherServer =
+            WeatherServer(temperature.timeOfDay, temperature.temp, precipitationAmount.precipitationAmount)
+        return weatherServer
+    }
+
+}
+
+
+fun main() {
+
+    val weatherServer = WeatherServer()
+    val temperature = Temperature("Утро", 2)
+    val precipitationAmount = PrecipitationAmount("утро", 30)
+
+    val sentSever = weatherServer.sentServerSMS(temperature, precipitationAmount)
+
+}
