fd684bbe21c270c98d4436f3145e85f4b6b3122f
ttrifle <Bigtoolforyou!@>
2024-05-06 00:37:26 +0300

    lesson 16-4 done

--- src/main/kotlin/lesson16/lesson16_4.kt
+++ src/main/kotlin/lesson16/lesson16_4.kt
@@ -1,2 +1,39 @@
 package lesson16
 
+class Order {
+
+    private val orderId: Int = 0
+    var isOrderReady = false
+
+    fun changeStatus(manager: Boolean): Boolean {
+        if (manager) {
+            isOrderReady = !isOrderReady
+            println("Статус готовности заказа поменялся на $isOrderReady")
+        } else
+            println("Менеджер не согласовал смену статуса заказа")
+
+        return isOrderReady
+    }
+}
+
+class Manager {
+
+    val name = String()
+
+    fun resolveManager(): Boolean {
+        val managerAnswer = true
+        println("Ответ менеджера на запрос согласования $managerAnswer")
+        return managerAnswer
+    }
+
+}
+
+
+fun main() {
+
+    val order = Order()
+    val manager = Manager()
+
+    order.changeStatus(manager.resolveManager())
+
+}
