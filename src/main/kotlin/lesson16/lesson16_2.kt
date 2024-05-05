936518a9c632d2c1152e64f38ea7078a2682bf8c
ttrifle <Bigtoolforyou!@>
2024-05-05 22:38:08 +0300

    lesson 16-2 done

--- src/main/kotlin/lesson16/lesson16_2.kt
+++ src/main/kotlin/lesson16/lesson16_2.kt
@@ -1,2 +1,21 @@
 package lesson16
 
+
+class Circle3(private val radius: Double) {
+
+    private val piDecimal: Double = 3.14
+
+    fun getPerimeter() = 2 * piDecimal * radius
+
+    fun getSquare() = radius * piDecimal
+
+}
+
+fun main() {
+
+    val circle3 = Circle3(3.0)
+
+    println(circle3.getPerimeter())
+    println(circle3.getSquare())
+
+}
