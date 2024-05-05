cac3ba9ce445257db42864c859f947bed4de76ea
ttrifle <Bigtoolforyou!@>
2024-05-05 20:18:51 +0300

    lesson 15-3 changed

--- src/main/kotlin/lesson15/lesson15_3.kt
+++ src/main/kotlin/lesson15/lesson15_3.kt
@@ -1,33 +1,11 @@
 package lesson15
 
-abstract class ForumMembers5 : Userable {
+abstract class User {
 
     abstract val userId: Int
     abstract val username: String
     abstract val password: String
     abstract val dateOfBirth: String
-
-    fun loginForum() {
-        println("Пользователь авторизирован")
-    }
-}
-
-class User(
-    override val userId: Int,
-    override val username: String,
-    override val password: String,
-    override val dateOfBirth: String,
-) : ForumMembers5()
-
-class Admin(
-    override val userId: Int,
-    override val username: String,
-    override val password: String,
-    override val dateOfBirth: String,
-    val isAdmin: Boolean = true,
-) : ForumMembers5(), Adminable
-
-interface Userable {
 
     fun readMessage() {
         println("Сообщение прочитано")
@@ -39,7 +17,20 @@ interface Userable {
 
 }
 
-interface Adminable {
+class Member(
+    override val userId: Int,
+    override val username: String,
+    override val password: String,
+    override val dateOfBirth: String,
+) : User()
+
+class Admin(
+    override val userId: Int,
+    override val username: String,
+    override val password: String,
+    override val dateOfBirth: String,
+    val isAdmin: Boolean = true,
+) : User() {
 
     fun deleteMessage() {
         println("Сообщение удалено")
@@ -51,19 +42,16 @@ interface Adminable {
 
 }
 
-
 fun main() {
     val admin = Admin(1, "1", "1", "1")
-    val user = User(2, "2", "2", "2")
+    val member = Member(2, "2", "2", "2")
 
-    admin.loginForum()
     admin.readMessage()
     admin.writeMessage()
     admin.deleteUser()
     admin.deleteMessage()
 
-    user.loginForum()
-    user.readMessage()
-    user.writeMessage()
+    member.readMessage()
+    member.writeMessage()
 
 }
