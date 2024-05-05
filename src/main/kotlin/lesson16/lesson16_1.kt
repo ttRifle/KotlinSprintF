5e9df43a05abb64630dc5639a269842d13be6585
ttrifle <Bigtoolforyou!@>
2024-05-05 22:09:22 +0300

    lesson 16-1 done

--- src/main/kotlin/lesson16/lesson16_1.kt
+++ src/main/kotlin/lesson16/lesson16_1.kt
@@ -1,2 +1,17 @@
 package lesson16
 
+class Cube2 {
+
+    private val throwResult: Int = (1..6).random()
+
+    fun getThrowResult() = throwResult
+
+}
+
+fun main() {
+
+    val cube2 = Cube2()
+
+    println(cube2.getThrowResult())
+
+}
