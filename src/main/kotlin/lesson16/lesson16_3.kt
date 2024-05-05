d30fe88b5b7e8ce6bd54656ae92aca8569d06a47
ttrifle <Bigtoolforyou!@>
2024-05-05 23:21:07 +0300

    lesson 16-3 done

--- src/main/kotlin/lesson16/lesson16_3.kt
+++ src/main/kotlin/lesson16/lesson16_3.kt
@@ -1,2 +1,19 @@
 package lesson16
 
+class User123 {
+
+    val userName: String = String()
+    private val userPass: String = "123"
+
+    fun checkPass(readPass: String) = readPass == userPass
+
+}
+
+fun main() {
+
+    val user123 = User123()
+    val readPass = "123"
+
+    println(user123.checkPass(readPass))
+
+}
