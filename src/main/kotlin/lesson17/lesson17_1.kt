e8785dec0b2f5cd17e11d95b08ab7ef564b61051
ttrifle <Bigtoolforyou!@>
2024-05-06 20:05:05 +0300

    lesson 17-1 done

--- src/main/kotlin/lesson17/lesson17_1.kt
+++ src/main/kotlin/lesson17/lesson17_1.kt
@@ -1,2 +1,14 @@
 package lesson17
 
+class Quiz {
+
+    val question: String = String()
+        get() = field
+
+    var answer: String = String()
+        get() = field
+        set(value) {
+            field = value
+        }
+
+}
