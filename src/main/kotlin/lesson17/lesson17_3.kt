c55c14ebcafe73e816933968c2f240830161ea9d
ttrifle <Bigtoolforyou!@>
2024-05-06 21:00:33 +0300

    lesson 17-3 done

--- src/main/kotlin/lesson17/lesson17_3.kt
+++ src/main/kotlin/lesson17/lesson17_3.kt
@@ -1,2 +1,34 @@
 package lesson17
 
+class Folder {
+
+    var name: String = "Алегрова"
+        get() {
+
+            if (isSecretFlag) {
+                field = "Скрытая папка"
+                println("$field, количество файлов 0")
+                return field
+            } else {
+                println("Папка $field, количество файлов $numberOfFiles")
+                return field
+            }
+
+        }
+
+
+    val numberOfFiles: Int = 10
+    var isSecretFlag: Boolean = false
+
+}
+
+fun main() {
+
+    val folder = Folder()
+
+    var a = folder.name
+    folder.isSecretFlag = true
+    a = folder.name
+
+
+}
