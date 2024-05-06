a224a4c2199a5232d0f1faee6a07ccdc205570f8
ttrifle <Bigtoolforyou!@>
2024-05-06 22:21:50 +0300

    lesson 17-5 done

--- src/main/kotlin/lesson17/lesson17_5.kt
+++ src/main/kotlin/lesson17/lesson17_5.kt
@@ -1,2 +1,36 @@
 package lesson17
 
+class User4(login: String, password: String) {
+
+    var login: String = login
+        set(value) {
+            field = value
+            println("Новый логин $field")
+        }
+    var password: String = password
+        get() {
+            var passToString = ""
+            for (i in 1..field.length) {
+                passToString += "*"
+            }
+            return passToString
+        }
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+
+}
+
+
+fun main() {
+
+    val user4 = User4("login", "password")
+
+    user4.password = "123"
+
+    println(user4.password)
+
+    user4.login = "Boris"
+
+
+}
