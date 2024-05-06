2cc5dd0c0d9f35d5159ef5337b4d0bbed5fab137
ttrifle <Bigtoolforyou!@>
2024-05-06 21:19:32 +0300

    lesson 17-4 done

--- src/main/kotlin/lesson17/lesson17_4.kt
+++ src/main/kotlin/lesson17/lesson17_4.kt
@@ -1,2 +1,23 @@
 package lesson17
 
+class Package1 {
+
+    val number: Int = 0
+    var currentLocation = "Отделение почты 1"
+        set(value) {
+            field = value
+            changeLocationCount += 1
+        }
+    var changeLocationCount = 0
+
+
+}
+
+fun main() {
+
+    val package1 = Package1()
+
+    package1.currentLocation = "Отделение почты 1"
+    println(package1.changeLocationCount)
+
+}
