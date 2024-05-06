11434488eeee4065f52b96d3e755cef89570d862
ttrifle <Bigtoolforyou!@>
2024-05-06 20:33:59 +0300

    lesson 17-2 done

--- src/main/kotlin/lesson17/lesson17_2.kt
+++ src/main/kotlin/lesson17/lesson17_2.kt
@@ -1,2 +1,23 @@
 package lesson17
 
+class Ship1 {
+
+    var name: String = "Аврора"
+        set(value) {
+            if (value != field)
+                println("Нельзя")
+        }
+    val averageSpeed: Int = 0
+    val motherPort: String = "Спб"
+
+}
+
+fun main() {
+
+    val ship1 = Ship1()
+
+    ship1.name = "1"
+    println(ship1.name)
+
+}
+
