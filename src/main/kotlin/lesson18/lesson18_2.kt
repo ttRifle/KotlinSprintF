c63b4981b3310e2b9f5a108877eb4a7569d74683
ttrifle <Bigtoolforyou!@>
2024-05-07 19:06:13 +0300

    lesson 18-2 done

--- src/main/kotlin/lesson18/lesson18_2.kt
+++ src/main/kotlin/lesson18/lesson18_2.kt
@@ -1,2 +1,52 @@
 package lesson18
 
+open class Cube {
+
+    open fun getThrowResult() = ""
+
+}
+
+
+class FourCube() : Cube() {
+
+    private val numberOfFace = 4
+    override fun getThrowResult(): String {
+        return "кубик c с количеством граней $numberOfFace: Результат броска ${(1..numberOfFace).random()}"
+
+    }
+
+}
+
+class SixCube() : Cube() {
+    private val numberOfFace = 6
+    override fun getThrowResult(): String {
+        return "кубик c с количеством граней $numberOfFace: Результат броска ${(1..numberOfFace).random()}"
+    }
+}
+
+class EightCube() : Cube() {
+    private val numberOfFace = 8
+    override fun getThrowResult(): String {
+        return "кубик c с количеством граней $numberOfFace: Результат броска ${(1..numberOfFace).random()}"
+    }
+}
+
+fun main() {
+
+    val fourCube: Cube = FourCube()
+    val sixCube: Cube = SixCube()
+    val eightCube: Cube = EightCube()
+
+    val arrayOfCubes = arrayOf<Cube>(fourCube, sixCube, eightCube)
+
+    getAllResults(arrayOfCubes)
+
+}
+
+fun getAllResults(cubes: Array<Cube>) {
+
+    cubes.forEach {
+        println(it.getThrowResult())
+    }
+
+}
