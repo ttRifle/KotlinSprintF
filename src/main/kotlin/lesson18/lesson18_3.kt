f899971e0bac73ee57512b1a3ae630f737e49e94
ttrifle <Bigtoolforyou!@>
2024-05-07 22:10:52 +0300

    lesson 18-3 done

--- src/main/kotlin/lesson18/lesson18_3.kt
+++ src/main/kotlin/lesson18/lesson18_3.kt
@@ -1,2 +1,67 @@
 package lesson18
 
+open class Animal(
+    open val name: String = String(),
+) {
+    open val eatType: String = ""
+
+    open fun sleep() {}
+
+    open fun eat() = ""
+
+
+}
+
+
+class Fox(
+    override val name: String,
+) : Animal() {
+
+    override val eatType: String = "Ягода"
+
+    override fun eat(): String {
+        return "[$name]->[$eatType]"
+
+    }
+
+}
+
+class Dog(
+    override val name: String
+) : Animal() {
+
+    override val eatType: String = "Кость"
+
+    override fun eat(): String {
+        return "[$name]->[$eatType]"
+    }
+}
+
+class Cat(
+    override val name: String
+) : Animal() {
+
+    override val eatType: String = "Рыба"
+
+    override fun eat(): String {
+        return "[$name]->[$eatType]"
+    }
+}
+
+fun main() {
+
+    val fox: Animal = Fox("Лиса")
+    val dog: Animal = Dog("Собака")
+    val cat: Animal = Cat("Кошка")
+    val listOfAnimals: List<Animal> = listOf(fox, dog, cat)
+
+    getAllResults(listOfAnimals)
+
+}
+
+fun getAllResults(animals: List<Animal>) {
+
+    for (element in animals) println(element.eat())
+
+}
+
