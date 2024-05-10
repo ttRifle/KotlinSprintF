710a30a22de9171140aff081b08c322b3f504538
ttrifle <Bigtoolforyou!@>
2024-05-10 16:19:59 +0300

    lesson 19-2 changed

--- src/main/kotlin/lesson19/lesson19_2.kt
+++ src/main/kotlin/lesson19/lesson19_2.kt
@@ -5,27 +5,21 @@ enum class Category {
     STATIONARY,
     OTHERS;
 
-    fun getCategoryName(category: Category): String {
-        return when (category) {
-            CLOTHES -> "Одежда"
-            STATIONARY -> "Канцелярские товары"
-            OTHERS -> "Другое"
-        }
+    fun getCategoryName(): String = this.name
 
-    }
 }
 
 class GOOD33(_name: String, _id: Int, _category: Category) {
 
     val name = _name
-    val id = _id
-    val category = _category
+    private val id = _id
+    private val category = _category
 
     fun printGood() {
         println()
         println("id: $id")
         println("название: $name")
-        println("Категория: ${Category.OTHERS.getCategoryName(category)}")
+        println("Категория: ${category.getCategoryName()}")
     }
 
 }
@@ -36,8 +30,6 @@ fun main() {
     val good11 = GOOD33("Куртка", 0, Category.CLOTHES)
     val good22 = GOOD33("Диск", 1, Category.OTHERS)
     val good33 = GOOD33("Дырокол", 2, Category.STATIONARY)
-
-
 
     good11.printGood()
     good22.printGood()
