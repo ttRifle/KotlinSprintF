3c99f1b6ba8561aaa75e1ff66f62d8e643e58ba8
ttrifle <Bigtoolforyou!@>
2024-05-10 20:43:24 +0300

    lesson 20-2 done

--- src/main/kotlin/lesson20/lesson20_2.kt
+++ src/main/kotlin/lesson20/lesson20_2.kt
@@ -1,2 +1,23 @@
 package lesson20
 
+class Player(_name: String, _currentHealth: Int, _maxHealth: Int) {
+
+    val name = _name
+    var currentHealth = _currentHealth
+    val maxHealth = _maxHealth
+
+}
+
+fun main() {
+
+    val player = Player("user", 62, 100)
+
+    val heal: (Player) -> Unit = fun(player: Player) {
+        player.currentHealth += player.maxHealth - player.currentHealth
+    }
+
+    println(player.currentHealth)
+
+    heal(player)
+
+}
