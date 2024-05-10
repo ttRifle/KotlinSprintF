1e49254943c1f924bf4ad94fedbd57aa0ebb4f0a
ttrifle <Bigtoolforyou!@>
2024-05-10 20:59:38 +0300

    lesson 20-3 done

--- src/main/kotlin/lesson20/lesson20_3.kt
+++ src/main/kotlin/lesson20/lesson20_3.kt
@@ -1,2 +1,22 @@
 package lesson20
 
+class Player321(_hasKey: Boolean) {
+
+    val hasKey = _hasKey
+
+}
+
+fun main() {
+
+    val player = Player321(true)
+
+    val openDoor: () -> Unit = fun() {
+
+        if (player.hasKey) println("Ты открыл дверь")
+        else println("дверь заперта")
+
+    }
+
+    openDoor()
+
+}
