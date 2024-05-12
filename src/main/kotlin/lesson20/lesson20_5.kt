f33d17384fc5dad14c8223cc61dd98a1ccce881f
ttrifle <Bigtoolforyou!@>
2024-05-12 18:37:08 +0300

    lesson 20-5 done

--- src/main/kotlin/lesson20/lesson20_5.kt
+++ src/main/kotlin/lesson20/lesson20_5.kt
@@ -1,2 +1,37 @@
 package lesson20
 
+class Robot {
+
+    private val listOfPhrases = listOf(
+        "Начинать всегда стоит с того, что сеет сомнения",
+        "Настоящая ответственность бывает только личной",
+        "Неосмысленная жизнь не стоит того, чтобы жит",
+        "80% успеха - это появиться в нужном месте в нужное время",
+        "Ваше время ограничено, не тратьте его, живя чужой жизнью"
+    )
+
+    private var currentPhrase: String = listOfPhrases[listOfPhrases.indices.random()]
+
+    fun say() {
+        println(currentPhrase)
+    }
+
+    fun setModifier(reverseWords: (String) -> String) {
+        currentPhrase = reverseWords(currentPhrase)
+    }
+
+}
+
+fun main() {
+
+    val robot = Robot()
+
+    val reverseWords: (String) -> (String) = { phrase: String -> phrase.split(" ").reversed().joinToString(" ") }
+
+    robot.say()
+
+    robot.setModifier(reverseWords)
+
+    robot.say()
+
+}
