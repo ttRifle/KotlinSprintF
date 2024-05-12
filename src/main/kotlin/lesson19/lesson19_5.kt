f7f42d3ec5860c49457afe598aedb4d86038ebcf
ttrifle <Bigtoolforyou!@>
2024-05-12 19:34:10 +0300

    lesson 19-5 done

--- src/main/kotlin/lesson19/lesson19_5.kt
+++ src/main/kotlin/lesson19/lesson19_5.kt
@@ -1,53 +1,55 @@
 package lesson19
 
-enum class Gender(val ruGender: String) {
+enum class Gender(
+    val ruGender: String
+) {
 
     MALE("Мужчина"),
-    FEMALE("Женщина");
+    FEMALE("Женщина"),
+    UNDEFINED("Не указан"),
 
-    fun getRuGender() = ruGender
 
 }
 
 class HumanCard(_name: String, _gender: Gender?) {
 
     val name = _name
-    val gender = _gender
+    private val gender = _gender
 
     fun printCard() {
-        println("Имя: $name, пол: ${gender?.getRuGender()}")
+        println("Имя: $name, пол: ${gender?.ruGender}")
     }
 
 }
 
 fun main() {
 
-    val humanCardList: List<HumanCard> = addCard()
+    val humanCardList: List<HumanCard> = createCardList()
 
-    humanCardList.indices.forEach() {
+    humanCardList.indices.forEach {
         humanCardList[it].printCard()
     }
 
 }
 
-fun getGenderFromString(gender: String?): Gender? {
+fun getGenderFromString(gender: String): Gender {
 
     return when {
-        gender.equals(gender, ignoreCase = true) -> Gender.MALE
-        gender.equals(gender, ignoreCase = true) -> Gender.FEMALE
-        else -> null
+        gender.equals(Gender.MALE.ruGender, ignoreCase = true) -> Gender.MALE
+        gender.equals(Gender.FEMALE.ruGender, ignoreCase = true) -> Gender.FEMALE
+        else -> Gender.UNDEFINED
     }
 
 }
 
-fun addCard(): List<HumanCard> {
+fun createCardList(): List<HumanCard> {
 
     val numberOfCards = 5
     val humanCardList: MutableList<HumanCard> = mutableListOf()
 
     println(
         "Необходимо заполнить имя и пол человека в одну строку через пробел. пример: \"Борис Мужчина\"\n" +
-                "Список полов ${Gender.entries}"
+                "Значения полов: [${Gender.MALE.ruGender},${Gender.FEMALE.ruGender}]"
     )
 
     for (i in 1..numberOfCards) {
