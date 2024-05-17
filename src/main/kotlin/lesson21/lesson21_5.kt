d599bdcbe1b2de5c958458de93fa88b92518c381
ttrifle <Bigtoolforyou!@>
2024-05-17 21:56:25 +0300

    lesson 21-5 done

--- src/main/kotlin/lesson21/lesson21_5.kt
+++ src/main/kotlin/lesson21/lesson21_5.kt
@@ -1,2 +1,12 @@
 package lesson21
 
+fun main() {
+
+    val experience: Map<String, Int> = mapOf("pistol" to 0, "machine gun" to 1000, "knife" to 1000, "rifle" to 200)
+
+    fun Map<String, Int>.maxCategory(): String =
+        (this.filterValues { it == this.maxOf { max -> max.value } }).keys.max().toString()
+
+    println(experience.maxCategory())
+
+}
