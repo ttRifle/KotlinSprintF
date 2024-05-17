13cceb089eb93e97d2f32be6a1d178f4a841e5f7
ttrifle <Bigtoolforyou!@>
2024-05-17 17:32:38 +0300

    lesson 21-2 this added

--- src/main/kotlin/lesson21/lesson21_2.kt
+++ src/main/kotlin/lesson21/lesson21_2.kt
@@ -1,2 +1,11 @@
 package lesson21
 
+fun main() {
+
+    val listOfNumbers = listOf(1, 2, 3, 4, 5, 6)
+
+    fun List<Int>.evenNumbersSum() = filter { number -> number % 2 == 0 }.sum()
+
+    println(listOfNumbers.evenNumbersSum())
+
+}
