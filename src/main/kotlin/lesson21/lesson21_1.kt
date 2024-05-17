767e97add3d787021f6f1df4c582f2c781dbb6a0
ttrifle <Bigtoolforyou!@>
2024-05-17 17:04:57 +0300

    lesson 21-1 done

--- src/main/kotlin/lesson21/lesson21_1.kt
+++ src/main/kotlin/lesson21/lesson21_1.kt
@@ -1,2 +1,22 @@
 package lesson21
 
+fun main() {
+
+    val word = "Hello"
+
+    fun String.vowelCount(): Int {
+        val vowel = "aoeiuy"
+        var count = 0
+
+        word.forEach {
+            for (i in vowel.indices) {
+                if (vowel[i].equals(it, ignoreCase = true)) count++
+            }
+        }
+
+        return count
+    }
+
+    println(word.vowelCount())
+
+}
