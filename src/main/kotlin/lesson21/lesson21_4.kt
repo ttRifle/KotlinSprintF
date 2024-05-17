923142eb0aeb224985311c86a32f85f1a6d2e967
ttrifle <Bigtoolforyou!@>
2024-05-17 18:46:34 +0300

    lesson 21-4 done

--- src/main/kotlin/lesson21/lesson21_4.kt
+++ src/main/kotlin/lesson21/lesson21_4.kt
@@ -1,2 +1,24 @@
 package lesson21
 
+import java.io.File
+
+fun main() {
+
+    val file = File("file.txt")
+    val text = "123"
+
+    fun File.appendLowerCaseTextToBeginning(text: String) {
+
+        val currentLines = file.readLines()
+
+        this.writeText(text)
+
+        currentLines.forEach() {
+            file.appendText(it)
+        }
+
+    }
+
+    file.appendLowerCaseTextToBeginning(text)
+
+}
