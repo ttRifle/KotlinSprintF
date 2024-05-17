8bf42d345c13860b334aa8ff9e0b66d44ca693c0
ttrifle <Bigtoolforyou!@>
2024-05-17 18:48:52 +0300

    lesson 21-4 done

--- src/main/kotlin/lesson21/lesson21_4.kt
+++ src/main/kotlin/lesson21/lesson21_4.kt
@@ -5,13 +5,13 @@ import java.io.File
 fun main() {
 
     val file = File("file.txt")
-    val text = "123"
+    val text = "F123F"
 
     fun File.appendLowerCaseTextToBeginning(text: String) {
 
         val currentLines = file.readLines()
 
-        this.writeText(text)
+        this.writeText(text.lowercase())
 
         currentLines.forEach() {
             file.appendText(it)
