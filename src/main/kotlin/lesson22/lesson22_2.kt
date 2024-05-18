5816eb8ee65c05b69c802c71f22cad51f35b81e1
ttrifle <Bigtoolforyou!@>
2024-05-18 18:52:59 +0300

    lesson 22-2 done

--- src/main/kotlin/lesson22/lesson22_2.kt
+++ src/main/kotlin/lesson22/lesson22_2.kt
@@ -1,2 +1,22 @@
 package lesson22
 
+class RegularBook22(
+    val name: String,
+    val author: String,
+)
+
+data class DataBook22(
+    val name: String,
+    val author: String,
+)
+
+fun main() {
+
+    val regularBook = RegularBook22("dictionary", "Mark")
+
+    val dataBook = DataBook22("dictionary", "Mark")
+
+    println("${regularBook}, тк у обычных классов по умолчанию toString выводит название класса")
+    println("${dataBook}, тк у дата классов по умолчанию toString выводит название класса и его поля")
+
+}
