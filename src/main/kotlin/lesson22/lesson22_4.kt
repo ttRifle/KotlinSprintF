4941aa52c9960281f481cdcc5ab60ffb9ab279c0
ttrifle <Bigtoolforyou!@>
2024-05-19 12:32:14 +0300

    lesson 22-4

--- src/main/kotlin/lesson22/lesson22_4.kt
+++ src/main/kotlin/lesson22/lesson22_4.kt
@@ -1,2 +1,41 @@
 package lesson22
 
+class ViewModel {
+
+    var mainScreenState: MainScreenState? = null
+
+    data class MainScreenState(
+        val isLoading: Boolean = false,
+        val data: String = "",
+    )
+
+    fun loadData() {
+
+        if (mainScreenState != null) {
+            if (mainScreenState?.isLoading == false) {
+                println("Загрузка данных")
+                mainScreenState = mainScreenState?.copy(isLoading = true)
+            } else {
+                println("Наличие загруженных данных")
+                mainScreenState = mainScreenState?.copy(isLoading = false)
+            }
+        } else {
+            println("Данные отсутствуют")
+        }
+
+    }
+}
+
+fun main() {
+
+    val viewModel = ViewModel()
+
+    viewModel.loadData()
+
+    viewModel.mainScreenState = ViewModel.MainScreenState(true, "some data")
+
+    viewModel.loadData()
+    viewModel.loadData()
+
+}
+
