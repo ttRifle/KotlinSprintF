f68d6582ccbf74236dcd1e32cec9d9180476f2a0
ttrifle <Bigtoolforyou!@>
2024-05-19 10:52:14 +0300

    lesson 22-1 done

--- src/main/kotlin/lesson22/lesson22_1.kt
+++ src/main/kotlin/lesson22/lesson22_1.kt
@@ -14,11 +14,10 @@ fun main() {
 
     val regularBook = RegularBook("dictionary", "Mark")
     val regularBook1 = RegularBook("dictionary", "Mark")
-
     val dataBook = DataBook("dictionary", "Mark")
     val dataBook2 = DataBook("dictionary", "Mark")
 
-    println("${regularBook == regularBook1}, тк у обычных классов сравниваются ссылки на объекты в памяти, а не их значения ")
-    println("${dataBook == dataBook2}, тк у дата классов сравниваются значения объектов")
+    println(regularBook == regularBook1) // будет false, тк у обычных классов сравниваются ссылки на объекты в памяти, а не их значения
+    println(dataBook == dataBook2) // будет true, тк у дата классов сравниваются значения объектов
 
 }
