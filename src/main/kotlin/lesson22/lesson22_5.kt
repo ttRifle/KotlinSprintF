9c09d0fbd6b0069f018819e2cc8cce395a19f483
ttrifle <Bigtoolforyou!@>
2024-05-19 13:22:12 +0300

    lesson 22-5

--- src/main/kotlin/lesson22/lesson22_5.kt
+++ src/main/kotlin/lesson22/lesson22_5.kt
@@ -1,2 +1,22 @@
 package lesson22
 
+data class GalacticGuide(
+    val placeOrEvent: String,
+    val descriptionOfPlaceOrEvent: String,
+    val dateAndTime: String?,
+    val placeOrEventDistanceFromEarth: String,
+) {
+
+    override fun toString(): String {
+        return "GalacticGuide(place='$placeOrEvent', descriptionOfPlace='$descriptionOfPlaceOrEvent', placeDistanceFromEarth='$placeOrEventDistanceFromEarth')"
+    }
+
+}
+
+fun main() {
+
+    val galacticGuide = GalacticGuide("Alpha Centauri", "Stellar system", "01.02.23 15:30", "4,36 light years")
+
+    println(galacticGuide)
+
+}
