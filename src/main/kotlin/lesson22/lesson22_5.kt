fa3af542f9f6daa98f4b251697c84089eca3f541
ttrifle <Bigtoolforyou!@>
2024-05-20 20:33:48 +0300

    lesson 22-5

--- src/main/kotlin/lesson22/lesson22_5.kt
+++ src/main/kotlin/lesson22/lesson22_5.kt
@@ -7,17 +7,10 @@ data class GalacticGuide(
     val placeOrEventDistanceFromEarth: String,
 )
 
-data class Planet(
-    val place: String,
-    val descriptionOfPlace: String,
-    val placeDistanceFromEarth: String,
-)
-
 fun main() {
 
     val galacticGuide = GalacticGuide("Alpha Centauri", "Stellar system", "01.02.23 15:30", "4,36 light years")
-    val planet = Planet(galacticGuide.component1(), galacticGuide.component2(), galacticGuide.component4())
 
-    println(planet)
+    println("Название места: ${galacticGuide.component1()}, Описание места: ${galacticGuide.component2()}, Расстояние места от земли: ${galacticGuide.component4()}")
 
 }
